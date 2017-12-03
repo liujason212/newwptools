7cc9e4574d33e3c682f19387eb7d6cefd7e34b02
liujason212 <jason6666@gmail.com>
2017-12-03 22:42:12 +0800

    修复了，验证结果时候，判断条件未生效的问题。

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from app import app
 from raven.contrib.flask import Sentry
 sentry = Sentry(app, dsn='https://e7efa4e39f6d4d69956fb1577d6b5fda:8d9feb63cc0840169c42fc01e7d25d20@sentry.io/235185')
-app.run(host='0.0.0.0', port=5000)
+app.run(host='127.0.0.1', port=5000)
 #app.run(debug=True,port=5007)
